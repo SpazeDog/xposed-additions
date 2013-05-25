f795fc1d8e298ad70c462ab0850408f2303c2054
Daniel Bergløv <d.bergloev@gmail.com>
2013-05-25 09:47:31 +0200

    Use internal onPreferenceClick()

--- src/com/spazedog/xposed/additions/SettingsInjector.java
+++ src/com/spazedog/xposed/additions/SettingsInjector.java
@@ -15,7 +15,7 @@ import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceChangeListener {
+public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceChangeListener, OnPreferenceClickListener {
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
     	if (lpparam.packageName.equals("com.android.settings")) {
     		findAndHookMethod("com.android.settings.DisplaySettings", lpparam.classLoader, "onCreate", android.os.Bundle.class, new XC_MethodHook() {
@@ -31,14 +31,14 @@ public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceCha
 	    			checkBoxPreferencePower.setKey("powerDisable");
 	    			checkBoxPreferencePower.setTitle("Disable power button");
 	    			checkBoxPreferencePower.setSummary("Do not turn on the screen when the power button is pressed");
-	    			checkBoxPreferencePower.setOnPreferenceClickListener((OnPreferenceClickListener) param.thisObject);
+	    			checkBoxPreferencePower.setOnPreferenceClickListener(SettingsInjector.this);
 	    			checkBoxPreferencePower.setChecked( Settings.System.getInt(context.getContentResolver(), "disable_power_button", 0) == 1 );
 	    			
 	    			CheckBoxPreference checkBoxPreferenceRotation = new CheckBoxPreference(context);
 	    			checkBoxPreferenceRotation.setKey("lockscreenRotation");
 	    			checkBoxPreferenceRotation.setTitle("Include lockscreen rotation");
 	    			checkBoxPreferenceRotation.setSummary("Include display rotation settings in the lock screen");
-	    			checkBoxPreferenceRotation.setOnPreferenceClickListener((OnPreferenceClickListener) param.thisObject);
+	    			checkBoxPreferenceRotation.setOnPreferenceClickListener(SettingsInjector.this);
 	    			checkBoxPreferenceRotation.setChecked( Settings.System.getInt(context.getContentResolver(), "enable_lockscreen_rotation", 0) == 1 );
 	    			
 	    			ListPreference listPreferencePlugged = new ListPreference(context);
@@ -55,22 +55,6 @@ public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceCha
 	    			screen.addPreference(listPreferencePlugged);
 	    		}
     		});
-    		
-    		findAndHookMethod("com.android.settings.DisplaySettings", lpparam.classLoader, "onPreferenceTreeClick", PreferenceScreen.class, Preference.class, new XC_MethodHook() {
-	    		@Override
-	    		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-	    			if (((Preference) param.args[1]).getKey().equals("powerDisable")) {
-	    				Settings.System.putInt(((PreferenceFragment) param.thisObject).getActivity().getContentResolver(), "disable_power_button", ((CheckBoxPreference) param.args[1]).isChecked() ? 1 : 0);
-	    				
-	    				param.setResult(true);
-	    				
-	    			} else if (((Preference) param.args[1]).getKey().equals("lockscreenRotation")) {
-	    				Settings.System.putInt(((PreferenceFragment) param.thisObject).getActivity().getContentResolver(), "enable_lockscreen_rotation", ((CheckBoxPreference) param.args[1]).isChecked() ? 1 : 0);
-	    				
-	    				param.setResult(true);
-	    			}
-	    		}
-    		});
     	}
 	}
 	
@@ -79,5 +63,21 @@ public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceCha
 		Settings.System.putString(preference.getContext().getContentResolver(), "xposed_wakeon_usb_change", (String) newValue);
 
 		return true;
+	}
+
+	@Override
+	public boolean onPreferenceClick(Preference preference) {
+		if (preference.getKey().equals("powerDisable")) {
+			Settings.System.putInt(preference.getContext().getContentResolver(), "disable_power_button", ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+			
+			return true;
+			
+		} else if (preference.getKey().equals("lockscreenRotation")) {
+			Settings.System.putInt(preference.getContext().getContentResolver(), "enable_lockscreen_rotation", ((CheckBoxPreference) preference).isChecked() ? 1 : 0);
+			
+			return true;
+		}
+		
+		return false;
 	}
 }
