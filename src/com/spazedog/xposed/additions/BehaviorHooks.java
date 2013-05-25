0b33bbf198a428c543a5cca625e3df8f1dc66345
Daniel Bergløv <d.bergloev@gmail.com>
2013-05-25 14:05:55 +0200

    Restructure code base

--- src/com/spazedog/xposed/additions/BehaviorHooks.java
+++ src/com/spazedog/xposed/additions/BehaviorHooks.java
@@ -11,40 +11,45 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class BehaviorHooks implements IXposedHookLoadPackage {
+	
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
     	if (lpparam.packageName.equals("android")) {
-	    	findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader, "interceptKeyBeforeQueueing", android.view.KeyEvent.class, Integer.TYPE, Boolean.TYPE, new XC_MethodHook() {
-	    		@Override
-	    		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-	    			if ( ((KeyEvent) param.args[0]).getKeyCode() == KeyEvent.KEYCODE_POWER && !((Boolean) param.args[2])) {
-	    				if (Settings.System.getInt(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_disable_power_button", 0) == 1) {
-	    					param.setResult(0);
-	    				}
-	    			}
-	    		}
-    		});
-    		
-    		findAndHookMethod("com.android.internal.policy.impl.keyguard.KeyguardViewManager", lpparam.classLoader, "shouldEnableScreenRotation", new XC_MethodReplacement() {
-				@Override
-				protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-					return Settings.System.getInt(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_enable_lockscreen_rotation", 0) == 1;
-				}
-    		});
-
-    		findAndHookMethod("com.android.server.power.PowerManagerService", lpparam.classLoader, "shouldWakeUpWhenPluggedOrUnpluggedLocked", Boolean.TYPE, Integer.TYPE, Boolean.TYPE, new XC_MethodHook() {
-				@Override
-				protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-					String actions = Settings.System.getString(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_wakeon_usb_change");
-					
-					if (actions == null || (actions.contains("pluggedin") && !((Boolean) param.args[0])) || (actions.contains("pluggedout") && ((Boolean) param.args[0]))) {
-						// Make sure that shouldWakeUpWhenPluggedOrUnpluggedLocked() does not stop the device from waking
-						XposedHelpers.setBooleanField(param.thisObject, "mWakeUpWhenPluggedOrUnpluggedConfig", true);
-						
-					} else {
-						param.setResult(false);
-					}
-				}
-    		});
+	    	findAndHookMethod("com.android.internal.policy.impl.PhoneWindowManager", lpparam.classLoader, "interceptKeyBeforeQueueing", android.view.KeyEvent.class, Integer.TYPE, Boolean.TYPE, PhoneWindowManager_interceptKeyBeforeQueueing);
+    		findAndHookMethod("com.android.internal.policy.impl.keyguard.KeyguardViewManager", lpparam.classLoader, "shouldEnableScreenRotation", KeyguardViewManager_shouldEnableScreenRotation);
+    		findAndHookMethod("com.android.server.power.PowerManagerService", lpparam.classLoader, "shouldWakeUpWhenPluggedOrUnpluggedLocked", Boolean.TYPE, Integer.TYPE, Boolean.TYPE, PowerManagerService_shouldWakeUpWhenPluggedOrUnpluggedLocked);
     	}
     }
+    
+    public final XC_MethodHook PhoneWindowManager_interceptKeyBeforeQueueing = new XC_MethodHook() {
+		@Override
+		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+			if ( ((KeyEvent) param.args[0]).getKeyCode() == KeyEvent.KEYCODE_POWER && !((Boolean) param.args[2])) {
+				if (Settings.System.getInt(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_disable_power_button", 0) == 1) {
+					param.setResult(0);
+				}
+			}
+		}
+    };
+    
+    public final XC_MethodHook PowerManagerService_shouldWakeUpWhenPluggedOrUnpluggedLocked = new XC_MethodHook() {
+		@Override
+		protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+			String actions = Settings.System.getString(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_wakeon_usb_change");
+			
+			if (actions == null || (actions.contains("pluggedin") && !((Boolean) param.args[0])) || (actions.contains("pluggedout") && ((Boolean) param.args[0]))) {
+				// Make sure that shouldWakeUpWhenPluggedOrUnpluggedLocked() does not stop the device from waking
+				XposedHelpers.setBooleanField(param.thisObject, "mWakeUpWhenPluggedOrUnpluggedConfig", true);
+				
+			} else {
+				param.setResult(false);
+			}
+		}
+    };
+    
+    public final XC_MethodReplacement KeyguardViewManager_shouldEnableScreenRotation = new XC_MethodReplacement() {
+		@Override
+		protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+			return Settings.System.getInt(((Context) XposedHelpers.getObjectField(param.thisObject, "mContext")).getContentResolver(), "xposed_enable_lockscreen_rotation", 0) == 1;
+		}
+    };
 }
--- src/com/spazedog/xposed/additions/SettingsInjector.java
+++ src/com/spazedog/xposed/additions/SettingsInjector.java
@@ -16,47 +16,51 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class SettingsInjector implements IXposedHookLoadPackage, OnPreferenceChangeListener, OnPreferenceClickListener {
+	
 	public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
     	if (lpparam.packageName.equals("com.android.settings")) {
-    		findAndHookMethod("com.android.settings.DisplaySettings", lpparam.classLoader, "onCreate", android.os.Bundle.class, new XC_MethodHook() {
-	    		@Override
-	    		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-	    			PreferenceScreen screen = ((PreferenceFragment) param.thisObject).getPreferenceScreen();
-	    			Context context = ((PreferenceFragment) param.thisObject).getActivity();
-	    			
-	    			PreferenceCategory preferenceCategory = new PreferenceCategory(context);
-	    			preferenceCategory.setTitle("Xposed Additions");
-	    			
-	    			CheckBoxPreference checkBoxPreferencePower = new CheckBoxPreference(context);
-	    			checkBoxPreferencePower.setKey("xposed_pref_disable_power_button");
-	    			checkBoxPreferencePower.setTitle("Disable power button");
-	    			checkBoxPreferencePower.setSummary("Do not turn on the screen when the power button is pressed");
-	    			checkBoxPreferencePower.setOnPreferenceClickListener(SettingsInjector.this);
-	    			checkBoxPreferencePower.setChecked( Settings.System.getInt(context.getContentResolver(), "xposed_disable_power_button", 0) == 1 );
-	    			
-	    			CheckBoxPreference checkBoxPreferenceRotation = new CheckBoxPreference(context);
-	    			checkBoxPreferenceRotation.setKey("xposed_pref_enable_lockscreen_rotation");
-	    			checkBoxPreferenceRotation.setTitle("Include lockscreen rotation");
-	    			checkBoxPreferenceRotation.setSummary("Include display rotation settings in the lock screen");
-	    			checkBoxPreferenceRotation.setOnPreferenceClickListener(SettingsInjector.this);
-	    			checkBoxPreferenceRotation.setChecked( Settings.System.getInt(context.getContentResolver(), "xposed_enable_lockscreen_rotation", 0) == 1 );
-	    			
-	    			ListPreference listPreferencePlugged = new ListPreference(context);
-	    			listPreferencePlugged.setKey("xposed_pref_wakeon_usb_change");
-	    			listPreferencePlugged.setEntryValues(new String[]{"", "pluggedin|pluggedout", "pluggedin", "pluggedout"});
-	    			listPreferencePlugged.setEntries(new String[]{"Never", "Always", "Plugged", "Unplugged"});
-	    			listPreferencePlugged.setTitle("Wake on Plugged/Unplugged");
-	    			listPreferencePlugged.setSummary("Wake the device when the USB is plugged or unplugged");
-	    			listPreferencePlugged.setOnPreferenceChangeListener(SettingsInjector.this);
-	    			
-	    			screen.addPreference(preferenceCategory);
-	    			screen.addPreference(checkBoxPreferencePower);
-	    			screen.addPreference(checkBoxPreferenceRotation);
-	    			screen.addPreference(listPreferencePlugged);
-	    		}
-    		});
+    		findAndHookMethod("com.android.settings.DisplaySettings", lpparam.classLoader, "onCreate", android.os.Bundle.class, DisplaySettings_onCreate);
     	}
 	}
+	
+	public final XC_MethodHook DisplaySettings_onCreate = new XC_MethodHook() {
+		@Override
+		protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+			PreferenceScreen screen = ((PreferenceFragment) param.thisObject).getPreferenceScreen();
+			Context context = ((PreferenceFragment) param.thisObject).getActivity();
+			
+			PreferenceCategory preferenceCategory = new PreferenceCategory(context);
+			preferenceCategory.setTitle("Xposed Additions");
+			
+			CheckBoxPreference checkBoxPreferencePower = new CheckBoxPreference(context);
+			checkBoxPreferencePower.setKey("xposed_pref_disable_power_button");
+			checkBoxPreferencePower.setTitle("Disable power button");
+			checkBoxPreferencePower.setSummary("Do not turn on the screen when the power button is pressed");
+			checkBoxPreferencePower.setOnPreferenceClickListener(SettingsInjector.this);
+			checkBoxPreferencePower.setChecked( Settings.System.getInt(context.getContentResolver(), "xposed_disable_power_button", 0) == 1 );
+			
+			CheckBoxPreference checkBoxPreferenceRotation = new CheckBoxPreference(context);
+			checkBoxPreferenceRotation.setKey("xposed_pref_enable_lockscreen_rotation");
+			checkBoxPreferenceRotation.setTitle("Include lockscreen rotation");
+			checkBoxPreferenceRotation.setSummary("Include display rotation settings in the lock screen");
+			checkBoxPreferenceRotation.setOnPreferenceClickListener(SettingsInjector.this);
+			checkBoxPreferenceRotation.setChecked( Settings.System.getInt(context.getContentResolver(), "xposed_enable_lockscreen_rotation", 0) == 1 );
+			
+			ListPreference listPreferencePlugged = new ListPreference(context);
+			listPreferencePlugged.setKey("xposed_pref_wakeon_usb_change");
+			listPreferencePlugged.setEntryValues(new String[]{"", "pluggedin|pluggedout", "pluggedin", "pluggedout"});
+			listPreferencePlugged.setEntries(new String[]{"Never", "Always", "Plugged", "Unplugged"});
+			listPreferencePlugged.setTitle("Wake on Plugged/Unplugged");
+			listPreferencePlugged.setSummary("Wake the device when the USB is plugged or unplugged");
+			listPreferencePlugged.setOnPreferenceChangeListener(SettingsInjector.this);
+			listPreferencePlugged.setValue( Settings.System.getString(context.getContentResolver(), "xposed_wakeon_usb_change") );
+			
+			screen.addPreference(preferenceCategory);
+			screen.addPreference(checkBoxPreferencePower);
+			screen.addPreference(checkBoxPreferenceRotation);
+			screen.addPreference(listPreferencePlugged);
+		}
+	};
 	
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
