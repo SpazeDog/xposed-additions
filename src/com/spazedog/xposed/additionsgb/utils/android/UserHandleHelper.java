8dcd23910b6b5171978de75ee7a182846ae10f7b
Daniel Bergløv <d.bergloev@gmail.com>
2015-03-28 20:33:17 +0100

    Fix config writing issues

--- src/com/spazedog/xposed/additionsgb/utils/android/UserHandleHelper.java
+++ src/com/spazedog/xposed/additionsgb/utils/android/UserHandleHelper.java
@@ -11,12 +11,23 @@ public class UserHandleHelper {
 	public static UserHandle getCurrentUser() {
 		ReflectClass userHandleClass = ReflectClass.forName("android.os.UserHandle");
 		
+		/*
+		 * TODO: This some times returns the default -2 as the userid and we get an error about cross-communication. 
+		 * We need to build a proper way to detect current users. At the same time maybe add real multi-user support 
+		 * with support for multi-configurations based on current user.
+		 */
+		
 		if (userHandleClass != null) {
 			ReflectConstructor userHandleConstructor = userHandleClass.findConstructor(Match.BEST, Integer.TYPE);
 			ReflectField currentUserField = userHandleClass.findField("USER_CURRENT");
+			int userid = (Integer) currentUserField.getValue();
+			
+			if (userid < 0) {
+				userid = 0;
+			}
 			
 			if (userHandleConstructor != null && currentUserField != null) {
-				return (UserHandle) userHandleConstructor.invoke( currentUserField.getValue() );
+				return (UserHandle) userHandleConstructor.invoke( userid );
 			}
 		}
 		
